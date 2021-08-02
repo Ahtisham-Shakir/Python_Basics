9e9c52b5a588dcc45f4d9fb09f6c8f4735a647ef
Ahtisham <ahtishamshakir777@gmail.com>
2021-08-02 09:23:18 -0700

    Print Statements in Python

---
+++ Basic Programs/tut_6.py
@@ -0,0 +1,10 @@
+# this is single line comment
+"""this
+is
+Multi line
+comment"""
+print("My name is","Ahtisham")
+print("He is ")
+print("good boy")
+print("My Father name is", end=" ") # Every print statement show in new line
+print("Shakir")
