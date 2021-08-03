92a4a78c669906f5d93c9f4700219343ec2d3882
Ahtisham <ahtishamshakir777@gmail.com>
2021-08-03 08:39:04 -0700

    Variables, Data Types, Type Casting

---
+++ Basic Programs/tut_7.py
@@ -0,0 +1,18 @@
+var1="Hello "
+var2=4
+var3=37.5
+var4="Ahtisham"
+print(type(var1)) # type function tell the type of variable
+print(var2 + var3)
+print(var1 + var4)
+
+# type casting
+
+print(int(var3)+var2)
+
+#input from user
+print("Enter 1st number")
+num1 = input()     #input store in the variable as a string
+print("Enter 2nd number")
+num2= input()
+print("Sum of numbers is= ",int(num1) + int(num2))
