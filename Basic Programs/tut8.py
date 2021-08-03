69f7bbfd7421aba462764881675f5562c5fc6e41
Ahtisham <ahtishamshakir777@gmail.com>
2021-08-03 08:39:47 -0700

    String Slicing & Other Functions

---
+++ Basic Programs/tut8.py
@@ -0,0 +1,13 @@
+mystr = "Ahtisham is a good boy"
+print(mystr[0:9])   # print 0th index to 8th indext
+print(mystr[0:9:2])   # print 0th index to 8th indext with 1 character gap
+print(mystr[::-1])      # take default values -1 reverse the string
+
+print(len(mystr))  # print lenght of string
+print(mystr.isalnum())
+print(mystr.endswith("boy"))
+print(mystr.find("is"))
+print(mystr.count("a"))
+print(mystr.upper())
+print(mystr.lower())
+print(mystr.replace("is","are"))
