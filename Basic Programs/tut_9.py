b777bf8774fe7981b77afb18a8a84cdda922577c
Ahtisham <ahtishamshakir777@gmail.com>
2021-08-03 08:44:06 -0700

    Python List & List Functions

---
+++ Basic Programs/tut_9.py
@@ -0,0 +1,21 @@
+grocery = ["Harpic", "vim bar", "Dudrant", "Bhindi", 56]
+print(grocery)
+print(grocery[1:4])
+numbers= [10, 5, 12, 78, 40]
+numbers.sort()
+# numbers.reverse()
+print(numbers)
+print(max(numbers))
+
+numbers.append(100)  # add at the end
+# numbers.pop()    remove at the end
+print(numbers)
+#numbers.insert(2,33)
+#numbers.remove(100)
+
+#mutable = can change        e.g   list  numbers[2] = 39 valid
+#immutable = cannot change   e.g   tple   tp[2] = 39 not valid
+tp = (1,3,6)  #sytax of tple
+
+
+
