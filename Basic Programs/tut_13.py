3ff1eefca6af481e612cf2ec48be76c9517c7e5c
Ahtisham <ahtishamshakir777@gmail.com>
2021-08-04 12:10:26 -0700

    if, else, elif Conditions in Python

---
+++ Basic Programs/tut_13.py
@@ -0,0 +1,23 @@
+var1 = 15
+print("Enter a number")
+var2 = int(input())
+if var2>var1:
+    print("Greater")
+
+elif var2==var1:
+    print("Equal")
+
+else:
+    print("Lesser")
+  # Quiz
+
+print("Enter age")
+age = int(input())
+if age>18:
+    print("You can drive")
+elif age==18:
+    print("We will think about it")
+else:
+    print("You cannot drive")
+
+
