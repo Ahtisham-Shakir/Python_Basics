4144fbf2d154eabdddd9742be56d8326114ab15a
Ahtisham <ahtishamshakir777@gmail.com>
2021-08-06 11:50:31 -0700

    Open(), Read(), Readline(), For Reading File

---
+++ Basic Programs/tut_26.py
@@ -0,0 +1,25 @@
+# open function store file in a pointer f
+f= open("shaam.txt")
+
+# content = f.read()
+# content = f.read(3)
+# print(content)
+
+# Print character by character
+# for line in content:
+#     print(line)
+
+# Read single line and next print statement next line
+# print(f.readline())
+# print(f.readline())
+# print(f.readline())
+
+# Store all lines in a list
+# print(f.readlines())
+
+# print line by line
+# for line in f:
+#     print(line)
+
+f.close()
+
