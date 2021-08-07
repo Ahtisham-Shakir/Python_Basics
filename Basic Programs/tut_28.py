8db7965a9fd92a163b4ac0254bd80fd134983c1d
Ahtisham <ahtishamshakir777@gmail.com>
2021-08-07 10:59:46 -0700

    Writing & append to a file

---
+++ Basic Programs/tut_28.py
@@ -0,0 +1,17 @@
+# w is the write mode, we can write in the file
+# f = open("shaam.txt", "w")
+# f.write("Ahtisham is a good boy")
+
+# print number of character write
+# a=f.write("Ahtisham is a good boy")
+# print(a)
+
+# a is a append mode, add text at the end
+# f = open("shaam.txt", "a")
+# f.write("Ahtisham is a smart boy\n")
+
+# r+ is read and write mode
+# f = open("shaam.txt", "r+")
+# print(f.read())
+# f.write("Welcome")
+# f.close()
