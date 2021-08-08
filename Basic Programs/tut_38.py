eefa0f12c31dd31034e6a10a12b2bfb9d8db82ab
Ahtisham <ahtishamshakir777@gmail.com>
2021-08-08 00:21:39 -0700

    using Python External & Built-in Modules

---
+++ Basic Programs/tut_38.py
@@ -0,0 +1,13 @@
+import random
+# Produce random integer
+random_Number = random.randint(0,10)
+print(random_Number)
+
+# Print random number
+ran = random.random()
+print(ran)
+
+# Choose one item randomly from the list
+l = ["Ahtisham", "Shakir", "Naveeda", "Waleed", "Ayesha"]
+r= random.choice(l)
+print(r)
